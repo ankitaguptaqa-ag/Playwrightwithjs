090da815944460ac4bde83c5eacaa4cda7594e8e
Ankita Gupta <ankita@billte.ch>
2026-05-21 17:07:02 +0530

    changes in conf file

--- playwright.config.js
+++ playwright.config.js
@@ -60,7 +60,7 @@ export default defineConfig({
 
   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
   trace: 'on-first-retry',
-  headless: false,
+  headless: !!process.env.CI,
   //trace: 'on-first-retry',
   screenshot: 'only-on-failure',
   video: 'retain-on-failure',
